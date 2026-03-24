dde30b599afcc69e666d8a2762ea06f359acfcc6
todkarasmita4 <todkarasmita4@gmail.com>
2026-03-24 09:57:25 +0000

    commit

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-let current = "";
+let current="";
 
-const names = {
-  caesar: "Caesar Cipher",
-  vigenere: "Vigenère Cipher",
-  rail: "Rail Fence Cipher",
-  playfair: "Playfair Cipher",
-  rsa: "RSA",
-  diffie: "Diffie-Hellman"
+const data={
+  caesar:["Caesar Cipher","Shift letters","Used in Roman times"],
+  vigenere:["Vigenère Cipher","Keyword shifts","More secure"],
+  rail:["Rail Fence","Zig-zag pattern","Transposition"],
+  playfair:["Playfair","Pair encryption","Military use"]
 };
 
-/* OPEN LAB */
+document.getElementById("toggle").onclick=()=>{
+  document.body.classList.toggle("light");
+};
+
 function openLab(type){
-  current = type;
+  current=type;
   document.getElementById("lab").classList.remove("hidden");
-  document.getElementById("labTitle").innerText = names[type];
+
+  document.getElementById("labTitle").innerText=data[type][0];
+  document.getElementById("desc").innerText=data[type][1];
+  document.getElementById("extra").innerText=data[type][2];
 }
 
-/* CLOSE */
 function closeLab(){
   document.getElementById("lab").classList.add("hidden");
 }
 
-/* RUN */
 function run(mode){
-  let text = document.getElementById("text").value;
-  let key = document.getElementById("key").value;
-  let out = "";
+  let text=document.getElementById("text").value;
+  let key=document.getElementById("key").value;
+  let out="";
 
-  if(current==="caesar") out = mode==="enc"?caesarEncrypt(text,+key):caesarDecrypt(text,+key);
-  if(current==="vigenere") out = mode==="enc"?vigenereEncrypt(text,key):vigenereDecrypt(text,key);
-  if(current==="rail") out = mode==="enc"?railFenceEncrypt(text,+key):railFenceDecrypt(text,+key);
-  if(current==="playfair") out = mode==="enc"?playfairEncrypt(text,key):playfairDecrypt(text,key);
+  if(current==="caesar") out=mode==="enc"?caesarEncrypt(text,+key):caesarDecrypt(text,+key);
+  if(current==="vigenere") out=mode==="enc"?vigenereEncrypt(text,key):vigenereDecrypt(text,key);
+  if(current==="rail") out=mode==="enc"?railFenceEncrypt(text,+key):railFenceDecrypt(text,+key);
+  if(current==="playfair") out=mode==="enc"?playfairEncrypt(text,key):playfairDecrypt(text,key);
 
-  document.getElementById("output").innerText = out;
+  document.getElementById("output").innerText=out;
 }
